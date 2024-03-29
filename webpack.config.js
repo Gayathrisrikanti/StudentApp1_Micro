c89766772a9762b43daf7f2004e7c635030a14c9
gayathri srikanti <srikantigayathri42@gmail.com>
2024-03-29 18:20:46 -0400

    Update webpack and package.json for Netlify deployment

--- webpack.config.js
+++ webpack.config.js
@@ -2,66 +2,70 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
-module.exports = (_, argv) => ({
-  output: {
-    publicPath: "http://localhost:3000/",
-  },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
+module.exports = (_, argv) => {
+  const isProduction = process.env.NODE_ENV === 'production' || argv.mode === 'production';
+  const publicPath = isProduction
+    ? 'https://dulcet-longma-e49a65.netlify.app/' // Replace with your Netlify app URL
+    : 'http://localhost:3000/';
 
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
+  return {
+    output: {
+      publicPath: publicPath,
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
+    devServer: {
+      port: 3000,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
         },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
-      },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "studentapp1",
+        filename: "remoteEntry.js",
+        remotes: {},
+        exposes: {
+          './StudentForm': './src/StudentForm',
+        },
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: deps["react-dom"],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
+      new Dotenv()
     ],
-  },
-
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "studentapp1",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {
-        './StudentForm': './src/StudentForm',
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-    new Dotenv()
-  ],
-});
+  };
+};
